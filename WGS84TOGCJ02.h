68281be42bf0bb6578d79c5ba65ed40ecd0328bc
ReeSun <ree.sun.cn@hotmail.com>
2014-09-04 18:38:15 +0800

    /**  *  *  WGS-84 坐标系，是GPS系统所采用的坐标系。一切正常工作的GPS或GPS芯片所返回的坐标值都是这个坐标系下的数值。Google地图采用的卫星图也是按照这 坐标系摆  *  放的。  *
    ************************************************************************
    ******************************************************
     *
     *  GCJ-02 坐标系，是我天朝政府搞出来的加密坐标系，也常常被称为
    “火星坐标系”。包括（但可能不限于）高德地图在内的国内地图服务商采用它来绘制地图。Apple、
     *  Google 等国外公司在其道路地图中使用的也是高德的数据。BD-09 坐标系则是百度地图专用的坐标系。
     *
    
    ************************************************************************
    ******************************************************
     *
     * 本例子提供 标准坐标系和中国坐标系的相互转换
     *
     */

---
+++ WGS84TOGCJ02.h
@@ -0,0 +1,83 @@
+//
+//  WGS84TOGCJ02.h
+//  boluoyou
+//
+//  Created by sunbb on 14-9-3.
+//  Copyright (c) 2014年 yzdmtd. All rights reserved.
+//
+
+/**
+ *
+ *  WGS-84 坐标系，是GPS系统所采用的坐标系。一切正常工作的GPS或GPS芯片所返回的坐标值都是这个坐标系下的数值。Google地图采用的卫星图也是按照这坐标系摆
+ *  放的。
+ *
+ ******************************************************************************************************************************
+ *
+ *  GCJ-02 坐标系，是我天朝政府搞出来的加密坐标系，也常常被称为 “火星坐标系”。包括（但可能不限于）高德地图在内的国内地图服务商采用它来绘制地图。Apple、
+ *  Google 等国外公司在其道路地图中使用的也是高德的数据。BD-09 坐标系则是百度地图专用的坐标系。
+ *
+ ******************************************************************************************************************************
+ *  
+ * 本例子提供 标准坐标系和中国坐标系的相互转换,查看更多(https://github.com/reesun1130/WGS84TOGCJ02)
+ *
+ */
+
+#import <Foundation/Foundation.h>
+
+typedef struct
+{
+    double lat;
+    double lng;
+} Location;
+
+Location LocationMake(double lat, double lng);
+
+@interface WGS84TOGCJ02 : NSObject
+
+/**
+ *  判断是否已经超出中国范围
+ *
+ *  @param lat 纬度
+ *  @param lon 经度
+ *
+ *  @return 0否 1是
+ */
+int outOfChina(double lat, double lng);
+
+/**
+ *  标准坐标-》中国坐标
+ *
+ *  @param wgLoc 标准坐标
+ *
+ *  @return 中国坐标
+ */
+Location transformFromWGSToGCJ(Location wgLoc);
+
+/**
+ *  中国坐标-》标准坐标
+ *
+ *  @param gcLoc 中国坐标
+ *
+ *  @return 标准坐标
+ */
+Location transformFromGCJToWGS(Location gcLoc);
+
+/**
+ *  中国坐标-》百度坐标
+ *
+ *  @param gcLoc 中国坐标
+ *
+ *  @return 百度坐标
+ */
+Location bd_encrypt(Location gcLoc);
+
+/**
+ *  百度坐标-》中国坐标
+ *
+ *  @param gcLoc 百度坐标
+ *
+ *  @return 中国坐标
+ */
+Location bd_decrypt(Location bdLoc);
+
+@end
---
+++ WGS84TOGCJ02.m
@@ -0,0 +1,155 @@
+//
+//  WGS84TOGCJ02.m
+//  boluoyou
+//
+//  Created by sunbb on 14-9-3.
+//  Copyright (c) 2014年 yzdmtd. All rights reserved.
+//
+
+#import "WGS84TOGCJ02.h"
+
+const double a = 6378245.0;
+const double ee = 0.00669342162296594323;
+const double pi = 3.14159265358979324;
+
+@implementation WGS84TOGCJ02
+
+//+ (CLLocationCoordinate2D)transformFromWGSToGCJ:(CLLocationCoordinate2D)wgsLoc
+//{
+//    CLLocationCoordinate2D adjustLoc;
+//    
+//    if(outOfChina(wgsLoc.latitude,wgsLoc.longitude))
+//    {
+//        adjustLoc = wgsLoc;
+//    }
+//    else
+//    {
+//        double adjustLat = transformLat(wgsLoc.longitude - 105.0,wgsLoc.latitude - 35.0);
+//        double adjustLon = transformLon(wgsLoc.longitude - 105.0,wgsLoc.latitude - 35.0);
+//        double radLat = wgsLoc.latitude / 180.0 * pi;
+//        double magic = sin(radLat);
+//        
+//        magic = 1 - ee * magic * magic;
+//        
+//        double sqrtMagic = sqrt(magic);
+//        
+//        adjustLat = (adjustLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * pi);
+//        adjustLon = (adjustLon * 180.0) / (a / sqrtMagic * cos(radLat) * pi);
+//        adjustLoc.latitude = wgsLoc.latitude + adjustLat;
+//        adjustLoc.longitude = wgsLoc.longitude + adjustLon;
+//    }
+//    return adjustLoc;
+//}
+
+int outOfChina(double lat, double lng)
+{
+    if (lng < 72.004 || lng > 137.8347)
+        return 1;
+    
+    if (lat < 0.8293 || lat > 55.8271)
+        return 1;
+    
+    return 0;
+}
+
+double transformLat(double x, double y)
+{
+    double ret = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * sqrt(x > 0 ? x:-x);
+    ret += (20.0 * sin(6.0 * x * pi) + 20.0 *sin(2.0 * x * pi)) * 2.0 / 3.0;
+    ret += (20.0 * sin(y * pi) + 40.0 * sin(y / 3.0 * pi)) * 2.0 / 3.0;
+    ret += (160.0 * sin(y / 12.0 * pi) + 320 * sin(y * pi / 30.0)) * 2.0 / 3.0;
+    
+    return ret;
+}
+
+double transformLon(double x, double y)
+{
+    double ret = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * sqrt(x > 0 ? x:-x);
+    ret += (20.0 * sin(6.0 * x * pi) + 20.0 * sin(2.0 * x * pi)) * 2.0 / 3.0;
+    ret += (20.0 * sin(x * pi) + 40.0 * sin(x / 3.0 * pi)) * 2.0 / 3.0;
+    ret += (150.0 * sin(x / 12.0 * pi) + 300.0 * sin(x / 30.0 * pi)) * 2.0 / 3.0;
+    
+    return ret;
+}
+
+Location LocationMake(double lat, double lng)
+{
+    Location loc;
+    loc.lat = lat, loc.lng = lng;
+    
+    return loc;
+}
+
+/**
+ *  标准坐标-》中国坐标
+ */
+Location transformFromWGSToGCJ(Location wgLoc)
+{
+    Location mgLoc;
+    if (outOfChina(wgLoc.lat, wgLoc.lng))
+    {
+        mgLoc = wgLoc;
+        return mgLoc;
+    }
+    double dLat = transformLat(wgLoc.lng - 105.0, wgLoc.lat - 35.0);
+    double dLon = transformLon(wgLoc.lng - 105.0, wgLoc.lat - 35.0);
+    double radLat = wgLoc.lat / 180.0 * pi;
+    double magic = sin(radLat);
+    magic = 1 - ee * magic * magic;
+    double sqrtMagic = sqrt(magic);
+    dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * pi);
+    dLon = (dLon * 180.0) / (a / sqrtMagic * cos(radLat) * pi);
+    mgLoc.lat = wgLoc.lat + dLat;
+    mgLoc.lng = wgLoc.lng + dLon;
+    
+    return mgLoc;
+}
+
+/**
+ *  中国坐标-》标准坐标
+ */
+Location transformFromGCJToWGS(Location gcLoc)
+{
+    Location wgLoc = gcLoc;
+    Location currGcLoc, dLoc;
+    while (1) {
+        currGcLoc = transformFromWGSToGCJ(wgLoc);
+        dLoc.lat = gcLoc.lat - currGcLoc.lat;
+        dLoc.lng = gcLoc.lng - currGcLoc.lng;
+        if (fabs(dLoc.lat) < 1e-7 && fabs(dLoc.lng) < 1e-7) {  // 1e-7 ~ centimeter level accuracy
+            // Result of experiment:
+            //   Most of the time 2 iterations would be enough for an 1e-8 accuracy (milimeter level).
+            //
+            return wgLoc;
+        }
+        wgLoc.lat += dLoc.lat;
+        wgLoc.lng += dLoc.lng;
+    }
+    
+    return wgLoc;
+}
+
+///
+///  Transform GCJ-02 to BD-09
+///
+const double x_pi = 3.14159265358979324 * 3000.0 / 180.0;
+Location bd_encrypt(Location gcLoc)
+{
+    double x = gcLoc.lng, y = gcLoc.lat;
+    double z = sqrt(x * x + y * y) + 0.00002 * sin(y * x_pi);
+    double theta = atan2(y, x) + 0.000003 * cos(x * x_pi);
+    return LocationMake(z * cos(theta) + 0.0065, z * sin(theta) + 0.006);
+}
+
+///
+///  Transform BD-09 to GCJ-02
+///
+Location bd_decrypt(Location bdLoc)
+{
+    double x = bdLoc.lng - 0.0065, y = bdLoc.lat - 0.006;
+    double z = sqrt(x * x + y * y) - 0.00002 * sin(y * x_pi);
+    double theta = atan2(y, x) - 0.000003 * cos(x * x_pi);
+    return LocationMake(z * cos(theta), z * sin(theta));
+}
+
+@end
